c5e9b7814111e91f43a2ed395488b35a67815610
Eric Matte <eric.matte@hotmail.ca>
2022-03-01 18:03:22 -0500

    Use spawnSync

--- src/extension.ts
+++ src/extension.ts
@@ -2,31 +2,28 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-import { ProcessEnvOptions, spawn } from "child_process";
+import { ProcessEnvOptions, spawnSync } from "child_process";
 import { join } from "path";
 
 const extensionPath = vscode.extensions.getExtension("ericmatte.ruby-irb-autocomplete")!.extensionPath;
 const script = join(extensionPath, "./src/irb_suggest.rb");
 
-const execute = async (cmd: string, args: string[], options: ProcessEnvOptions): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    const spawned = spawn(cmd, args, options);
+const executeSync = (cmd: string, args?: string[], options?: ProcessEnvOptions): string => {
+  console.log("cmd:", `${cmd} ${(args || []).join(" ")}`);
+  const spawned = spawnSync(cmd, args, options);
+  console.log("done:", spawned.status);
 
-    let output = "";
-    spawned.stdout.on("data", data => {
-      output += data.toString();
-    });
+  if (spawned.error) {
+    throw spawned.error;
+  }
 
-    spawned.on("close", () => resolve(output));
-
-    spawned.on("error", error => reject(error));
-  });
+  return spawned.stdout.toString();
 };
 
-async function getIrbSuggestions(input: string): Promise<string[]> {
+const getIrbSuggestions = (input: string): string[] => {
   const cursor = "irb_output=";
   try {
-    const output = await execute(`bundle`, [`exec`, `spring`, `rails`, `r`, script], {
+    const output = executeSync(`bundle`, [`exec`, `spring`, `rails`, `r`, script], {
       env: { ...process.env, cursor, input },
     });
 
@@ -36,7 +33,7 @@ async function getIrbSuggestions(input: string): Promise<string[]> {
     console.error(error);
     throw error;
   }
-}
+};
 
 const getInput = (line: string) => {
   let input = line.substring(0, line.lastIndexOf("."));
@@ -46,11 +43,7 @@ const getInput = (line: string) => {
   return input;
 };
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "ruby-irb-autocomplete" is now active!');
-
   let lastSuggestion: { input: string; suggestions: vscode.CompletionItem[] } | undefined;
 
   const suggestionsToCompletionItems = (input: string, suggestions: string[]): vscode.CompletionItem[] => {
@@ -76,18 +69,17 @@ export function activate(context: vscode.ExtensionContext) {
   };
 
   let rubySuggest = vscode.languages.registerCompletionItemProvider("ruby", {
-    provideCompletionItems: async (document, position, token, context) => {
+    provideCompletionItems: (document, position, token, context) => {
       const line = document.lineAt(position).text.trim();
       let input = getInput(line);
 
       console.log(`Suggesting for '${input}'...`);
 
       if (lastSuggestion && lastSuggestion.input === input) {
-        console.log(`Returning cached suggestions for '${input}'`);
         return lastSuggestion.suggestions;
       }
 
-      const suggestions = await getIrbSuggestions(input);
+      const suggestions = getIrbSuggestions(input);
       return suggestionsToCompletionItems(input, suggestions);
     },
   });
@@ -96,4 +88,6 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  // executeSync("bundle", [`exec`, `spring`, `stop`]);
+}
